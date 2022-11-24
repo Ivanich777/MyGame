71ce1a886134dd24c581c78e492cd61bd6f441ff
Vanya <geo.vox95@gmail.com>
2022-11-24 16:12:05 +0300

    models

---
+++ db/migrations/20221124123400-create-card.js
@@ -0,0 +1,42 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Cards', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      answer: {
+        type: Sequelize.TEXT
+      },
+      question: {
+        type: Sequelize.TEXT
+      },
+      cost: {
+        type: Sequelize.INTEGER
+      },
+      topic_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Topics',
+          key: 'id',
+        },
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Cards');
+  }
+};
