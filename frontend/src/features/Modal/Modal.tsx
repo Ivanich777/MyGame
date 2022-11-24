8af407795fe095b1f3f2c090d1feaa003f824981
Vanya <geo.vox95@gmail.com>
2022-11-24 20:04:41 +0300

    modal

--- frontend/src/features/Modal/Modal.tsx
+++ frontend/src/features/Modal/Modal.tsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { Children, useState } from 'react'
+import './Modal.css'
 
-export const Modal = () => {
+export const Modal = (): JSX.Element => {
+  const [active, setActive] = useState(false);
+
   return (
-    <div className='modal'>Modal</div>
+    <>
+      <button onClick={() => setActive(true)} type='button'>Модалка</button>
+      <div className={active ? "modal active" : "modal"} onClick={() => setActive(false)}>
+        <div className={active ? "modal_content active" : "modal_content"} onClick={e => e.stopPropagation()}>
+          <form className='modal_form'>
+          <h2>Вопрос: Тут какой-то вопрос</h2>
+            <textarea className='modal_inpt' name='answer' placeholder='Ваш ответ' />
+            <button className='modal_btn' type='submit'>Ответить</button>
+          </form>
+        </div>
+      </div>
+    </>
   )
 }
